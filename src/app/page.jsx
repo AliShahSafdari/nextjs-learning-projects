328acee03daec77d3442a5c8d83f8f6dc91f23f6
Alishah Safdari <alishahsafdari905@gmail.com>
2025-03-07 21:59:49 +0430

    Example of API

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,7 +1,22 @@
-import Link from "next/link";
+async function getUser(){
+  let response = await fetch("https://jsonplaceholder.typicode.com/users")
+  let users = await response.json()
+  return users
+}
 
-export default function Home(){
-  return( <div>
-    <h1>This is the homepage</h1>
-  </div>
-)}
+export default async function showUser(){
+  let allUser = await getUser()
+  return(
+    <div>
+      <ul>
+        {
+          allUser.map(
+            (user, index)=>(
+              <li key={index}> {user.name}</li>
+            )
+          )
+        }
+      </ul>
+    </div>
+  )
+}

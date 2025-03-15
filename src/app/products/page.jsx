cd510ab3fd48ab86fa46dfb13640dff0a6e0501b
Alishah Safdari <alishahsafdari905@gmail.com>
2025-03-15 15:02:54 +0430

    Added a product

--- src/app/products/page.jsx
+++ src/app/products/page.jsx
@@ -4,6 +4,8 @@ import { useEffect, useState } from "react"
 
 export default function ProductPage(){
     const[products, setProducts] = useState([])
+    const[newname, setNewName] = useState("")
+    const[newprice, setNewPrice] = useState("")
 
     function fetchProduct (){
         fetch("http://localhost:3000/api/products")
@@ -11,6 +13,17 @@ export default function ProductPage(){
         .then(data => setProducts(data))
         .catch(error=> console.error(error))
     }
+
+    function handleAddProduct(){
+        fetch("http://localhost:3000/api/products", {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({name:newname, price:newprice})
+        })
+        .then(fetchProduct)
+        .catch(error=> console.error(error))
+    }
+
     useEffect(
         ()=> fetchProduct(), 
     [])
@@ -22,6 +35,18 @@ export default function ProductPage(){
                     (product, index)=> <li key={index}> {product.name} - {product.price}</li>
                 )}
             </ul>
+            <h2>Add New Product:</h2>
+            <input type="text" placeholder="Enter product name"
+             value={newname} onChange={()=> setNewName(event.target.value)} />
+            <br />
+            <input type="text" placeholder="Enter product price"
+              value={newprice} onChange={()=> setNewPrice(event.target.value)}/>
+            <br />
+            <button onClick={() =>{
+                handleAddProduct()
+                setNewName("")
+                setNewPrice("")
+            }}> Add new Product </button>
         </div>
     )
 }
